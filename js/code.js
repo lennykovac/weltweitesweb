f409c3f2d39f2b351ce0461f86606fd36ef175ac
Lenny Kovac <lennykovac@googlemail.com>
2021-09-15 17:29:09 +0200

    new attempt

--- js/code.js
+++ js/code.js
@@ -2,13 +2,13 @@ var s;
 var scl = 2;
 
 function setup() {
-	createCanvas(600,600);
-	background(255);
+  var canv = createCanvas(windowWidth, windowHeight);
+  canv.parent("canvas");
 	s = new Snake();	
 }
 
 function draw() {
-	translate(width / 2, height / 2);
+	translate(width / 4, height / 4);
 	s.update();
 	s.show();
 
@@ -31,6 +31,7 @@ function isPrime(value) {
 
 //simple snake object, which draws my line ;))
 function Snake() {
+  this.flip= true;
 	this.x = 0;
 	this.y = 0;
 	this.xspeed = 0;
@@ -39,37 +40,41 @@ function Snake() {
 	this.cnt = 0;
 	this.primes = isPrime(100000);
 	
-
 	this.update = function() {
 		this.cnt++;
 
 		if(this.cnt % 1000 === 0) {
-			alert(this.cnt);
+			console.log(this.cnt);
 		}
+
 		if(this.primes[this.cnt] === true) {
-			if(this.xspeed === 1 && this.yspeed === 0){ // right -> down
-				this.dir(0,1);
-			} else if(this.xspeed === 0 && this.yspeed === 1){ // down -> left
-				this.dir(-1,0);
-			} else if(this.xspeed === -1 && this.yspeed === 0){ // left -> up
-				this.dir(0,-1);
-			} else if(this.xspeed === 0 && this.yspeed === -1){ // up -> right
-				this.dir(1,0);
-			} 
+      if(this.flip === true) {
+        if(this.xspeed === 1 && this.yspeed === 0){ // right -> down
+          this.dir(0,1);
+        } else if(this.xspeed === 0 && this.yspeed === 1){ // down -> left
+          this.dir(-1,0);
+        } else if(this.xspeed === -1 && this.yspeed === 0){ // left -> up
+          this.dir(0,-1);
+        } else if(this.xspeed === 0 && this.yspeed === -1){ // up -> right
+          this.dir(1,0);
+        } 
+        this.flip = false;
+      }
+      else {
+        this.flip = true;
+      }
 		}
 		
-	
-
 		this.x = this.x + this.xspeed * scl;
 		this.y = this.y + this.yspeed * scl;
 		
-		this.x = constrain(this.x, 0, width-scl);
-		this.y = constrain(this.y, 0, height-scl);
+		//this.x = constrain(this.x, 0, width-scl);
+		//this.y = constrain(this.y, 0, height-scl);
 	}
 
 	this.show = function() {
 		fill(255);
-		rect(this.x, this.y, scl, scl);
+		point(this.x, this.y, scl);
 	}
 
 	this.dir = function(x, y) {
